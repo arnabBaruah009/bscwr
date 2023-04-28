aa8c3ac21495ee55643891943b930d2b6d2f0584
Arnab Baruah <baruaharnab769@gmail.com>
2023-04-28 15:56:07 +0530

    Creted list

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,46 @@
 import styles from "../styles/navbar.module.css";
+import Logo1 from "../images/Logo1.svg";
+import Logo2 from "../images/Logo2.svg";
 
 const Navbar = () => {
   return (
     <div className={styles.nav}>
-      <div className={styles.imgDiv}></div>
+      <div className={styles.imgDiv}>
+        <img src={Logo1} alt="Logo1"></img>
+        <img src={Logo2} alt="Logo2"></img>
+      </div>
       <div className={styles.navList}>
         <ul>
-            <li>Home</li>
-            <li>Team</li>
-            <li>Finance</li>
-            <li>Research Projects</li>
-            <li>Gallery</li>
-            <li>Publication</li>
+          <li>
+            <div>
+              <b>Home</b>
+            </div>
+          </li>
+          <li>
+            <div>
+              <b>Team</b>
+            </div>
+          </li>
+          <li>
+            <div>
+              <b>Finance</b>
+            </div>
+          </li>
+          <li>
+            <div>
+              <b>Research Projects</b>
+            </div>
+          </li>
+          <li>
+            <div>
+              <b>Gallery</b>
+            </div>
+          </li>
+          <li>
+            <div>
+              <b>Publication</b>
+            </div>
+          </li>
         </ul>
       </div>
     </div>
