c8b15aa6d814acc3bcbb5c36d3669aec16901b63
Arnab Baruah <baruaharnab769@gmail.com>
2023-04-28 22:30:18 +0530

    Added routes to all elements

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,23 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import { Navbar } from "./";
+import { Home, Team, Research, Gallery, Publications } from "../pages";
+
 function App() {
-  return <div className="App">Hello World!! Let's Begin!</div>;
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/team" element={<Team />}></Route>
+          <Route path="/research" element={<Research />}></Route>
+          <Route path="/gallery" element={<Gallery />}></Route>
+          <Route path="/publications" element={<Publications />}></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
