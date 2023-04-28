0f95f94eaa303457698f7e2e91bbfa5f560141b7
Arnab Baruah <baruaharnab769@gmail.com>
2023-04-28 22:29:07 +0530

    imported exported all pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,7 @@
 import Home from "./Home";
+import Team from "./Team";
+import Research from "./Research";
+import Gallery from "./Gallery";
+import Publications from "./Publications";
 
-export { Home };
+export { Home, Team, Research, Gallery, Publications };
