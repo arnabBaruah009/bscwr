0d52d895e0e587ab5fc60e064ce7785c3a87f4c1
Arnab Baruah <baruaharnab769@gmail.com>
2023-04-30 03:50:19 +0530

    imported and exported images

---
+++ src/images/gallery/index.js
@@ -0,0 +1,6 @@
+import image1 from "./image1.svg";
+import image2 from "./image2.svg";
+import image3 from "./image3.svg";
+import image4 from "./image4.svg";
+
+export { image1, image2, image3, image4 };
